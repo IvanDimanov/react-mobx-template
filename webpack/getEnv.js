ec347514338862ffa7796badd889bbf6a83aace8
Ivan Dimanov <ivan.dimanov@xogito.com>
2018-03-25 13:29:14 +0300

    Safe build

--- webpack/getEnv.js
+++ webpack/getEnv.js
@@ -28,6 +28,7 @@ const getEnv = () => {
      * and since we cannot commit the '.env' file
      * we conna fallback to use the default process env vars
      */
+    console.warn(`Unable to read file ${ENV_PATH} fallback using process.env:`, error)
     return process.env
   }
 
