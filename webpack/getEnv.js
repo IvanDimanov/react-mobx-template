fd4226b03c9184165da2051e460ca2442e093ee9
Ivan Dimanov <ivan.dimanov@xogito.com>
2018-03-25 12:17:41 +0300

    Heroku safe env vars

--- webpack/getEnv.js
+++ webpack/getEnv.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
+const ENV_PATH = path.resolve(__dirname, '../.env')
+
 const parse = (variable) => {
   if (!isNaN(Number(variable))) {
     return Number(variable)
@@ -15,37 +17,50 @@ const parse = (variable) => {
   return variable
 }
 
-const ENV_PATH = path.resolve(__dirname, '../.env')
-const envFile = fs.readFileSync(ENV_PATH, 'utf-8')
+const getEnv = () => {
+  let envFile
 
-/**
- * Why not use `process.env` directly or something like `` ?
- * => Because it `process.env` contains all environment variable
- * which the FE needs only the ones that are contained in the `ENV_PATH` file.
- *
- * Moreover, the values need to be `JSON.stringify()`
- * because of the `webpack.DefinePlugin()`
- * https://webpack.js.org/plugins/define-plugin/#usage
- */
-const env = envFile
-  .split('\n')
-  .filter((line) => line)  // Removes empty lines
-  .filter((line) => !line.startsWith('//'))  // Removes "//" comments
-  .filter((line) => !line.startsWith('#'))   // Removes "#" comments
-  .map((line) => {
-    const data = line.split('=')
-    const key = String(data[0]).trim()
-    let value = String(data[1]).trim()
-    value = JSON.stringify(parse(value))
+  try {
+    envFile = fs.readFileSync(ENV_PATH, 'utf-8')
+  } catch (error) {
+    /**
+     * Heroku cannot add files outside '.gitignore'
+     * and since we cannot commit the '.env' file
+     * we conna fallback to use the default process env vars
+     */
+    return process.env
+  }
 
-    return {
-      [key]: value
-    }
-  })
-  .reduce((item, obj) => ({ ...obj, ...item }), {})
+  /**
+   * Why not use `process.env` directly or something like `` ?
+   * => Because it `process.env` contains all environment variable
+   * which the FE needs only the ones that are contained in the `ENV_PATH` file.
+   *
+   * Moreover, the values need to be `JSON.stringify()`
+   * because of the `webpack.DefinePlugin()`
+   * https://webpack.js.org/plugins/define-plugin/#usage
+   */
+  const env = envFile
+    .split('\n')
+    .filter((line) => line)  // Removes empty lines
+    .filter((line) => !line.startsWith('//'))  // Removes "//" comments
+    .filter((line) => !line.startsWith('#'))   // Removes "#" comments
+    .map((line) => {
+      const data = line.split('=')
+      const key = String(data[0]).trim()
+      let value = String(data[1]).trim()
+      value = JSON.stringify(parse(value))
+
+      return {
+        [key]: value
+      }
+    })
+    .reduce((item, obj) => ({ ...obj, ...item }), {})
+}
 
 /**
  * Always provide only a clone but never the original
  * because objects can be altered
  */
+const env = getEnv()
 module.exports = () => Object.assign({}, env)
