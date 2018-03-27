b42f355de7c22ac9dc1838da865dbbaf6150a5f1
Ivan Dimanov <ivan.dimanov@xogito.com>
2018-03-27 15:41:49 +0300

    Revert

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const common = require('./webpack.config.common')
 const getEnv = require('./getEnv')
@@ -7,12 +8,8 @@ const getEnv = require('./getEnv')
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      ecma: 8,
-      compress: {
-        warnings: false
-      }
+    new UglifyJSPlugin({
+      sourceMap: true
     }),
 
     new webpack.DefinePlugin({
