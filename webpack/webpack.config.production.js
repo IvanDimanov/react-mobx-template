6e728a94b9c552022a75fe403e9123ca03ce31b6
Ivan Dimanov <ivan.dimanov@xogito.com>
2018-03-27 20:14:59 +0300

    Try to load the keys

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -1,22 +1,19 @@
-// const webpack = require('webpack')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const common = require('./webpack.config.common')
 const getEnv = require('./getEnv')
 
-console.log('env =', getEnv())
-
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
-    })
-    // ,
+    }),
 
-    // new webpack.DefinePlugin({
-    //   'process.env': getEnv()
-    // })
+    new webpack.DefinePlugin({
+      'process.env': getEnv()
+    })
   ]
 })
